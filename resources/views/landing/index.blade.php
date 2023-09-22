084dfd004633608f7e5d21a49f7f78c557becd19
Nadhiradhit <nadhiradhitya@gmail.com>
2023-09-22 13:02:18 +0700

    feat: rename folder, add folder layouts

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,6 +1,6 @@
-@extends('component.app')
+@extends('layouts.landing')
 
-@extends('component.navbar')
+@section('title', 'Dashboard')
 
 @section('content')
     <div class="w-full my-6">
@@ -9,73 +9,37 @@
             <div class="bg-white">
                 <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
                 <h1 class="font-bold text-2xl text-center text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">My Personal Branding</h1>
-                <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
                     <div class="group relative">
-                    <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                    <div class="aspect-h-1 aspect-w-1 w-full h-96 overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                        <img src="{{ URL('pic/img-2.jpeg')}}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
                     </div>
                     <div class="mt-4 flex justify-between">
                         <div>
-                        <h3 class="text-sm text-gray-700">
+                        <h3 class="text-md text-slate-800">
                             <a href="#">
                             <span aria-hidden="true" class="absolute inset-0"></span>
-                            Basic Tee
+                            Programming
                             </a>
                         </h3>
-                        <p class="mt-1 text-sm text-gray-500">Black</p>
+                        <p class="mt-1 text-sm text-gray-500">I </p>
                         </div>
-                        <p class="text-sm font-medium text-gray-900">$35</p>
                     </div>
                     </div>
                     <div class="group relative">
-                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                        <div class="aspect-h-1 aspect-w-1 w-full h-96 overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                        <img src="{{ URL('pic/img-3.jpeg')}}" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
                         </div>
                         <div class="mt-4 flex justify-between">
                         <div>
-                            <h3 class="text-sm text-gray-700">
+                            <h3 class="text-md text-slate-800">
                             <a href="#">
                                 <span aria-hidden="true" class="absolute inset-0"></span>
-                                Basic Tee
+                                Editor
                             </a>
                             </h3>
                             <p class="mt-1 text-sm text-gray-500">Black</p>
                         </div>
-                        <p class="text-sm font-medium text-gray-900">$35</p>
-                        </div>
-                    </div>
-                    <div class="group relative">
-                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
-                        </div>
-                        <div class="mt-4 flex justify-between">
-                        <div>
-                            <h3 class="text-sm text-gray-700">
-                            <a href="#">
-                                <span aria-hidden="true" class="absolute inset-0"></span>
-                                Basic Tee
-                            </a>
-                            </h3>
-                            <p class="mt-1 text-sm text-gray-500">Black</p>
-                        </div>
-                        <p class="text-sm font-medium text-gray-900">$35</p>
-                        </div>
-                    </div>
-                    <div class="group relative">
-                        <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
-                        </div>
-                        <div class="mt-4 flex justify-between">
-                        <div>
-                            <h3 class="text-sm text-gray-700">
-                            <a href="#">
-                                <span aria-hidden="true" class="absolute inset-0"></span>
-                                Basic Tee
-                            </a>
-                            </h3>
-                            <p class="mt-1 text-sm text-gray-500">Black</p>
-                        </div>
-                        <p class="text-sm font-medium text-gray-900">$35</p>
                         </div>
                     </div>
                 </div>
@@ -84,3 +48,4 @@
         </div>
     </div>
 @endsection
+
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Personal Data</title>
+    <title>@yield('title')</title>
 </head>
 <body>
+
+    <x-navbar-landing/>
+
     @yield('content')
 </body>
 </html>
